9e52ffa1d1693b6d7a31f810d194f074e4cade9d
preciousMzembe <preciousmzembe@gmail.com>
2022-09-21 01:16:03 +0900

    adding hive database

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_local_variable
 
 import 'package:classmate/pages/wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  var box = Hive.openBox("classmatebox");
   runApp(const MyApp());
 }
 
