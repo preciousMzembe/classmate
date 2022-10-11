6aeb05d7f7e13c155b1e4bb7e6f8eda5899c3fcc
21800627 <21800627@github.com>
2022-10-11 12:01:31 +0900

    Add subject and shows on timetable

--- lib/pages/Timetable.dart
+++ lib/pages/Timetable.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class Timetable extends StatefulWidget {
   const Timetable({super.key});
@@ -59,6 +60,10 @@ class GridBuilder extends StatefulWidget {
 }
 //https://stackoverflow.com/questions/43943946/how-do-i-use-async-http-data-to-return-child-widgets-in-an-indexedwidgetbuilder
 class GridBuilderState extends State<GridBuilder> {
+  final boxdata = Hive.box("classmatebox").toMap();
+  final _timeList = ['1 (8:30~9:45)', '2 (10:00~11:15)', '3 (11:30~12:45)', '4 (1:00~2:15)', '5 (2:30~3:45)', '6 (4:00~5:15)', '7 (5:30~6:45)', '8 (7:00~8:15)'];
+  final _days = ["", "Mon", "Tue", "Wed", "Thur", "Fri"];
+
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
@@ -67,14 +72,15 @@ class GridBuilderState extends State<GridBuilder> {
         gridDelegate:
         const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 6),
         itemBuilder: (_, int index) {
+          double row = index/6;
+
           // Header: Mon Tue Wed Thur Fri
           if(index < 6){
-            var days = ["", "Mon", "Tue", "Wed", "Thur", "Fri"];
             return InkWell(
               child: GridTile(
                   child: Center(
                     child: Text(
-                      "${days[index]}",
+                      "${_days[index]}",
                       style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     )),
@@ -83,10 +89,8 @@ class GridBuilderState extends State<GridBuilder> {
           }
           // Time : 1 2 3 4 5 6 7 8
           if(index%6==0){
-            double row = index/6;
             return InkWell(
               child: GridTile(
-
                   child: Center(
                       child: Text(
                         "${row.toInt()}",
@@ -98,26 +102,35 @@ class GridBuilderState extends State<GridBuilder> {
             );
           }
           // shows subject
-          if(widget.subjectList[index]){
-            return InkWell(
-              child: GridTile(
-                  child: Container(
-                    margin: EdgeInsets.all(3),
-                    padding: EdgeInsets.all(3),
-                    alignment: Alignment.center,
-                    child: Text("ICT APP Develop"),
-                    decoration:BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      )),
+          for (var element in boxdata.values) {
+            if (element['classTime'] == _timeList[row.toInt() -1] &&
+                element['title'] == "subject") {
+
+              for(var data in element["dayList"]){
+                if(data == _days[index%6]){
+                  return InkWell(
+                    child: GridTile(
+                      child: Container(
+                        margin: EdgeInsets.all(3),
+                        padding: EdgeInsets.all(3),
+                        alignment: Alignment.center,
+                        decoration:BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            )),
+                        child: Text(element["name"]),
+                      ),
                     ),
-                  ),
-            );
+                  );
+                }
+              }
+            }
           }
           return InkWell(
             child: GridTile(
                 child: Container(
+                  // child: Text(index.toString()),
                   decoration:BoxDecoration(
                     border: Border(bottom: BorderSide(color: Theme.of(context).dividerColor))
                 )),
