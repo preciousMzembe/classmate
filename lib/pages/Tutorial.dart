46f9c57abb468803ca11469a28dde655b6f3ca88
lhj82 <lhj8271@naver.com>
2022-10-14 11:49:08 +0900

    color

---
+++ lib/pages/Tutorial.dart
@@ -0,0 +1,88 @@
+import 'package:classmate/pages/intro_page_1.dart';
+import 'package:classmate/pages/intro_page_2.dart';
+import 'package:classmate/pages/intro_page_3.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class Tutorial extends StatefulWidget {
+  const Tutorial({Key? key}) : super(key: key);
+
+  @override
+  State<Tutorial> createState() => _TutorialState();
+}
+
+class _TutorialState extends State<Tutorial> {
+
+  //controller to keep track of which page we're on
+  PageController _controller = PageController();
+
+  //keep track of if we are on the last page or not
+  bool onLastPage = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          //page view
+          PageView(
+            controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                onLastPage = (index == 2);
+              });
+            },
+            children: [
+              IntroPage1(),
+              IntroPage2(),
+              IntroPage3(),
+            ],
+          ),
+
+          //dot indicators
+          Container(
+            alignment: Alignment(0,0.75),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //skip
+                GestureDetector(
+                  onTap: (){
+                    _controller.jumpToPage(2);
+                  },
+                  child: Text('skip'),
+                ),
+
+                //dot indicator
+                SmoothPageIndicator(controller: _controller, count: 3),
+                //next or done
+                onLastPage
+                    ? GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return HomePage();
+                        },
+                      ),
+                    );
+                  },
+                  child: Text('done'),
+                )
+                    : GestureDetector(
+                  onTap: () {
+                    _controller.nextPage(duration: Duration(milliseconds: 500), curve: Curves.easeIn,
+                    );
+                  },
+                  child: Text('next'),
+                )
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/intro_page_1.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class IntroPage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple,
+    );
+  }
+}
+
